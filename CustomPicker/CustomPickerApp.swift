df061404195cbe561682fa3ee694e6a36f3b3712
ibrahimgedami <ibrahim.abdelgani@seddiqiholding.com>
2024-05-26 12:13:25 +0400

    Initial Commit

---
+++ CustomPicker/CustomPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPickerApp.swift
+//  CustomPicker
+//
+//  Created by Ibrahim Gedami on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
